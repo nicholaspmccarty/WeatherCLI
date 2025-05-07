794abda857b7527559357e5d14134782989eeeb8
Nicholas McCarty <nicholasmccarty@Nicholass-Air.lan>
2025-05-07 14:08:22 -0400

    added api imports

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,23 @@
 package org.example;
 
 import java.util.Scanner;
+import com.google.gson.Gson;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 ///  Nicholas McCarty
 /// Simple CLI interface
 /// Gradle + gson + open-mateo api
 
 public class Main {
+
+    // Class variables for api and request handling
+    private static final String API_URL = "https://api.open-meteo.com/v1/forecast?latitude=%s&longitude=%s&current_weather=true";
+    private static final OkHttpClient client = new OkHttpClient();
+    private static final Gson gson = new Gson();
+
+
     public static void main(String[] args) {
 
         // Simple scanner for data input
@@ -21,4 +32,3 @@ public class Main {
 
         }
     }
-}

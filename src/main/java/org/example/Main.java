7ff5780956ce4fb000d33f6837b74dae087ed2c1
Nicholas McCarty <nicholasmccarty@Nicholass-Air.lan>
2025-05-07 14:06:36 -0400

    initialize scanner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,24 @@
 package org.example;
 
+import java.util.Scanner;
+
 ///  Nicholas McCarty
 /// Simple CLI interface
 /// Gradle + gson + open-mateo api
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Simple scanner for data input
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter longitude");
+
+        String longitude = scanner.nextLine();
+
+        System.out.println("Enter latitude");
+        String latitude = scanner.nextLine();
+
+
         }
     }
 }

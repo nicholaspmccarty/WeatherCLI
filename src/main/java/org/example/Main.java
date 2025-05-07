238fcdbd419a4c9420fa847b1d190cd534a2b647
Nicholas McCarty <nicholasmccarty@Nicholass-Air.lan>
2025-05-07 14:03:17 -0400

    initial doc

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
 package org.example;
 
+///  Nicholas McCarty
+/// Simple CLI interface
+/// Gradle + gson + open-mateo api
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
